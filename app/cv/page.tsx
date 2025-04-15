801cde04b694fceb62c73fff53b870595fde223c
mnapo <napolimatiase@gmail.com>
2025-04-15 19:09:46 -0300

    añadir metadata

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,3 +1,11 @@
+import type {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: "Porfolio Personal • Currículum",
+  description: "Porfolio personal de Matías Nápoli (2025)",
+  icons: "/favicon.svg",
+}
+
 export default function Page() {
   return (
     <div>
