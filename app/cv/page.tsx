2d373f14ee1066a59470d5a2b88c591c4ac5affe
mnapo <napolimatiase@gmail.com>
2025-04-24 18:46:21 -0300

    botones de descarga pdf/docx

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from 'next'
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/molecules/shadcn/avatar"
+import {Button} from "@/components/molecules/shadcn//button";
 
 export const metadata: Metadata = {
   title: "Porfolio Personal • Currículum",
@@ -10,7 +11,13 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <div>
-      <br></br><br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br>
+      <Button variant="default">
+        <p className="pl-1">pdf</p>
+      </Button>
+      <Button variant="default">
+        <p className="pl-1">docx</p>
+      </Button>
       <Avatar className="avatar">
         <AvatarImage src="cv/cv.jpg" />
         <AvatarFallback>Foto Perfil</AvatarFallback>
