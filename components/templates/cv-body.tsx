e6d5c54fdaa5b2ea329c02e95394fa532d21f247
mnapo <napolimatiase@gmail.com>
2025-04-25 15:58:58 -0300

    reducir separación

--- components/templates/cv-body.tsx
+++ components/templates/cv-body.tsx
@@ -2,16 +2,12 @@ import {Avatar, AvatarFallback, AvatarImage} from "@/components/molecules/shadcn
 
 export default function CV_Body() {
   return (
-    <section className="py-24 lg:py-32">
-      <div className="container mx-auto px-4 md:px-6 lg:px-8">
-        <div className="grid items-center gap-8 lg:grid-cols-2">
-          <div className="space-y-6">
+    <section className="py-1">
+      <div className="container">
             <Avatar className="avatar">
               <AvatarImage src="cv/cv.jpg" />
               <AvatarFallback>Foto Perfil</AvatarFallback>
             </Avatar>
-          </div>
-        </div>
       </div>
     </section>
   )
--- components/templates/cv-downloads.tsx
+++ components/templates/cv-downloads.tsx
@@ -2,7 +2,7 @@ import {Button} from "@/components/molecules/shadcn/button";
 
 export default function CV_Downloads() {
   return (
-    <section className="py-24 cv-downloads">
+    <section className="cv-downloads">
       <div className="container">
         <Button variant="default" className="btn-download">
           <p className="pl-1">PDF</p>
