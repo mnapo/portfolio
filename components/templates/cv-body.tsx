729346a16bafcf881c189f38ba0a574b503f43ae
mnapo <napolimatiase@gmail.com>
2025-04-25 16:25:50 -0300

    añadir secciones Estudios y Experiencia laboral

--- components/templates/cv-body.tsx
+++ components/templates/cv-body.tsx
@@ -6,11 +6,23 @@ export default function CV_Body() {
   return (
     <section className="py-1">
       <div className="container">
-            <Avatar className="avatar">
-              <AvatarImage src="cv/cv.jpg" />
-              <AvatarFallback>Foto Perfil</AvatarFallback>
-            </Avatar>
+        <Avatar className="avatar">
+          <AvatarImage src="cv/cv.jpg" />
+          <AvatarFallback>Foto Perfil</AvatarFallback>
+        </Avatar>
       </div>
+      <section className="py-4">
+        <div className="container mx-auto px-4 md:px-6 lg:px-8">
+          <h1>Estudios</h1>
+          <hr></hr>
+        </div>
+      </section>
+      <section className="py-4">
+        <div className="container mx-auto px-4 md:px-6 lg:px-8">
+          <h1>Experiencia laboral</h1>
+          <hr></hr>
+        </div>
+      </section>
     </section>
   )
 }
