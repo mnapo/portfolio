f3b1ad68b464932d4bd11430c6c73f247d60f081
mnapo <napolimatiase@gmail.com>
2025-04-25 15:53:06 -0300

    corregir alineación

--- components/templates/cv-downloads.tsx
+++ components/templates/cv-downloads.tsx
@@ -1,19 +1,15 @@
-import {Button} from "@/components/molecules/shadcn//button";
+import {Button} from "@/components/molecules/shadcn/button";
 
 export default function CV_Downloads() {
   return (
-    <section className="py-24 lg:py-32 cv-downloads">
-      <div className="container mx-auto px-4 md:px-6 lg:px-8">
-        <div className="grid items-center gap-8 lg:grid-cols-2">
-          <div className="space-y-6">
-            <Button variant="default" className="btn-download">
-              <p className="pl-1">PDF</p>
-            </Button>
-            <Button variant="default" className="btn-download">
-              <p className="pl-1">DOCX</p>
-            </Button>
-          </div>
-        </div>
+    <section className="py-24 cv-downloads">
+      <div className="container">
+        <Button variant="default" className="btn-download">
+          <p className="pl-1">PDF</p>
+        </Button>
+        <Button variant="default" className="btn-download">
+          <p className="pl-1">DOCX</p>
+        </Button>
       </div>
     </section>
   )
