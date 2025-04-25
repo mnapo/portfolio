439da0b8f8aeb07566e89ee402481a028e23008a
mnapo <napolimatiase@gmail.com>
2025-04-25 16:15:04 -0300

    remover cv.json y definir arreglos de estudios y experiencia en CV_Body

--- components/templates/cv-body.tsx
+++ components/templates/cv-body.tsx
@@ -1,6 +1,8 @@
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/molecules/shadcn/avatar"
 
 export default function CV_Body() {
+  const studies: { title: string; subtitle: string; description: string }[] = [];
+  const experience: { title: string; subtitle: string; description: string }[] = [];
   return (
     <section className="py-1">
       <div className="container">
