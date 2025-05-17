b4396407bb145744d50c5be25ef8dbfafb7efb22
mnapo <napolimatiase@gmail.com>
2025-05-17 02:07:49 -0300

    linkear a CV PDF

--- components/templates/cv-downloads.tsx
+++ components/templates/cv-downloads.tsx
@@ -1,12 +1,15 @@
 import {Button} from "@/components/molecules/shadcn/button";
+import Link from 'next/link'
 
 export default function CV_Downloads() {
   return (
     <section className="cv-downloads">
       <div className="container">
-        <Button variant="default" className="btn-download">
-          <p className="pl-1">PDF</p>
-        </Button>
+        <Link target="_blank" href="cv/CV MATIAS NAPOLI 2025.pdf">
+          <Button variant="default" className="btn-download">
+            <p className="pl-1">PDF</p>
+          </Button>
+        </Link>
         <Button variant="default" className="btn-download">
           <p className="pl-1">DOCX</p>
         </Button>
